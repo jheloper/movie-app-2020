fcfaa268eb4f86ff627af07a8b759c019767399f
Joonghyeon Kim <jheloper9102@gmail.com>
2020-07-06 17:55:35 +0900

    Potato => Food 컴포넌트로 변경, props를 사용하는 기본적인 예제 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-function Potato() {
-  return <h1>I like potato</h1>;
+function Food(props) {
+    console.log(props);
+    return <h1>I like {props.fav}</h1>;
 }
 
 function App() {
     return (
         <div>
             <h1>Hello</h1>
-            <Potato></Potato>
+            <Food fav="kimchi"></Food>
         </div>
     );
 }
