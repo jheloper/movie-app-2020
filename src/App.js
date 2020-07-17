32624ed80784b1945f06910b8bb0f1061a0d17f7
Joonghyeon Kim <jheloper9102@gmail.com>
2020-07-17 16:57:09 +0900

    Food 데이터에 id 추가하여 Food 컴포넌트의 key로 사용하도록 수정, img 요소 alt 속성 추가

--- src/App.js
+++ src/App.js
@@ -5,33 +5,38 @@ function Food(props) {
     return (
         <div>
             <h2>I like {name}</h2>
-            <img src={picture} />
+            <img src={picture} alt={name} />
         </div>
     );
 }
 
 const foodILike = [
     {
+        id: 1,
         name: "Kimchi",
         image:
             "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
     },
     {
+        id: 2,
         name: "Samgyepsal",
         image:
             "https://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg",
     },
     {
+        id: 3,
         name: "Bibimbap",
         image:
             "https://img.etoday.co.kr/pto_db/2019/07/600/20190726153503_1350707_1200_876.jpg",
     },
     {
+        id: 4,
         name: "Donkatsu",
         image:
             "https://m.yum-yumfood.com/web/product/big/201607/29_shop1_382074.jpg",
     },
     {
+        id: 5,
         name: "Kimbap",
         image:
             "https://homecuisine.co.kr/files/attach/images/140/001/083/558d170258752df2dd76bef00861497f.JPG",
@@ -39,7 +44,7 @@ const foodILike = [
 ];
 
 function renderFood(dish) {
-    return <Food name={dish.name} picture={dish.image}></Food>;
+    return <Food key={dish.id} name={dish.name} picture={dish.image}></Food>;
 }
 
 function App() {
